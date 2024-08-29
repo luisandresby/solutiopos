cceee8b6f2b043b8525c573a375a4a02279a2d37
Luis Andrés Bolaños Yapo <luisandresby@gmail.com>
2024-08-29 19:14:08 +0200

    page():

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="app"
+    :class="{
+      'app-sidebar-mobile-toggled': appOption.appSidebarMobileToggled,
+    }"
+  >
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
+  </div>
+</template>
+<script setup>
+const appOption = useAppOptionStore();
+</script>
+<style></style>
