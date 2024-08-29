a6a05ed85ac61be21c004448a8ee463d5e9403cf
Luis Andrés Bolaños Yapo <luisandresby@gmail.com>
2024-08-29 19:18:29 +0200

    feat(): Configured layouts for authenticated and non-authenticated view, login view, reset password, configured multi-language, base components.

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <NuxtLayout name="authenticated-layout"> </NuxtLayout>
+</template>
+
+<script setup>
+// 1. Imports
+import { createLogger } from "@/composables/useLogger";
+
+// 2. Name
+definePageMeta({
+  name: "index",
+  middleware: "auth",
+});
+
+// Head
+useHead({
+  title: "Index",
+});
+
+// 3. Composables
+const logger = createLogger();
+logger.debug("This is an debug message");
+
+// 4. Props
+const props = defineProps();
+
+// 5. Emits
+const emits = defineEmits();
+
+// 6. Data
+
+// 7. Computed
+
+// 8. Watchers
+
+// 9. Lifecycle hooks
+
+// 10. Methods
+</script>
+
+<style lang="scss"></style>
