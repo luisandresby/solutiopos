37cb3cd4012033988c6435f4bd343ca3c7468ba9
Luis Andrés Bolaños Yapo <luisandresby@gmail.com>
2024-08-29 19:10:00 +0200

    refactor(): error page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- BEGIN error -->
+  <div class="error">
+    <div class="error-code">404</div>
+    <div class="error-content">
+      <div class="error-message">{{ $t("page-error.404") }}</div>
+      <div class="error-desc mb-4">
+        <span v-html="$t('page-error.404-text')"></span>
+      </div>
+      <div>
+        <NuxtLink to="/auth/login" class="btn btn-success px-3 rounded-pill">
+          <span class="me-1">{{ $t("global.back") }}</span>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+  <!-- END error -->
+</template>
